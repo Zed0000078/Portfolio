15ae82b129a5ea02bbfa1599622cd2f933e112d3
Mohammad Zaid <89304051+Zed0000078@users.noreply.github.com>
2023-11-12 18:21:23 +0530

    Add files via upload

---
+++ portfolio page/script.js
@@ -0,0 +1,10 @@
+card = document.querySelector(".card");
+card.onclick = function(){
+    window.open("https://email-record.vercel.app/", "_blank")
+}
+
+card2 = document.querySelector(".card2");
+card2.onclick = function(){
+    window.open("https://first-react-project-olive.vercel.app/", "_blank")
+}
+
