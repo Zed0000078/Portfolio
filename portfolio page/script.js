1c457d8367efa16e0eeceabaf291bd665f8500a8
Mohammad Zaid <89304051+Zed0000078@users.noreply.github.com>
2024-01-28 22:23:53 +0530

    Update script.js

--- portfolio page/script.js
+++ portfolio page/script.js
@@ -8,3 +8,26 @@ card2.onclick = function(){
     window.open("https://first-react-project-olive.vercel.app/", "_blank")
 }
 
+
+
+
+li = document.querySelector(".linkedin")
+li.onclick = function(){
+    window.open("https://www.linkedin.com/in/mohammad-zaid-b04174206")
+}
+
+gh = document.querySelector(".github")
+gh.onclick = function(){
+    window.open("https://www.github.com/Zed0000078")
+}
+
+wa = document.querySelector(".whatsapp")
+wa.onclick = function(){
+    window.open("https://api.whatsapp.com/send?phone=7068204914")
+}
+
+res = document.querySelector(".resume")
+res.onclick = function(){
+    window.open('assets/resume.pdf')
+}
+
