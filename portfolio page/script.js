918e1708dfdc6752f1854b1ef65e3e466fa000ef
Mohammad Zaid <89304051+Zed0000078@users.noreply.github.com>
2024-07-28 18:54:05 +0530

    Update script.js

--- portfolio page/script.js
+++ portfolio page/script.js
@@ -28,6 +28,6 @@ wa.onclick = function(){
 
 res = document.querySelector(".resume")
 res.onclick = function(){
-    window.open('assets/MOHAMMAD-ZAID Resume.pdf')
+    window.open('assets/Mohammad Zaid Resume-p.pdf')
 }
 
